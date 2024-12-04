c347abf2c8e0f9d61f5c382b3c397af4704a0721
nathmankind <nathmankind@gmail.com>
2024-12-04 11:06:28 +0100

    update base url

--- utils/Provider/constants.ts
+++ utils/Provider/constants.ts
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:3200";
+export const BASE_URL =
+  "https://systematic-camille-nathaniel04-544610e9.koyeb.app"; //"http://localhost:3200";
 export const token_name = "tK_U_za";
